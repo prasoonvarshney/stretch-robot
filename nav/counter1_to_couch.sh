d5d61d421b858c7925506c2ec5fc80a47aa52430
jiminsun <jiminxsun@gmail.com>
2023-03-19 15:14:20 -0400

    add pipeline to deliver basket

--- nav/counter1_to_couch.sh
+++ nav/counter1_to_couch.sh
@@ -1,12 +1,16 @@
-python coord_goals_tepper.py -g 1 # go to counter1
+#!/bin/bash
 
-python arm_motion.py -loc counter1 -int open
-python arm_motion.py -loc counter1 -int close
-python arm_motion.py -loc retracted -int close
-python coord_goals_tepper.py -g 0 # go to couch
+python3 arm_motion.py -loc retracted -int rest  # wherever it starts, retract arm first
+python3 coord_goals_tepper.py -g 1 # go to counter1
 
-navigate to couch
-python arm_motion.py -loc couch -int close
-python arm_motion.py -loc couch -int open
-python arm_motion.py -loc retracted -int rest
-python coord_goals_tepper.py -g 1 # go back to counter1
+python3 arm_motion.py -loc retracted -int open
+python3 arm_motion.py -loc counter1 -int open
+python3 arm_motion.py -loc counter1 -int close
+python3 arm_motion.py -loc retracted -int carry
+python3 coord_goals_tepper.py -g 0 # go to couch
+
+python3 arm_motion.py -loc couch -int close  # extend arm
+python3 arm_motion.py -loc couch -int open  # place the basket
+python3 arm_motion.py -loc retracted -int open  # retract arm
+python3 arm_motion.py -loc retracted -int rest  # rest gripper
+python3 coord_goals_tepper.py -g 1 # go back to counter1
