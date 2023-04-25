400b4a37b61614261ebe4b23bce723309d7d2398
Prasoon Varshney <pvarshne@andrew.cmu.edu>
2023-04-25 00:24:39 -0400

    working reach to aruco functionality

--- aruco/nav_to_marker.py
+++ aruco/nav_to_marker.py
@@ -27,12 +27,22 @@ import actionlib
 from move_base_msgs.msg import MoveBaseGoal, MoveBaseAction
 
 
-ARM_LENGTH = 1  # meters?
+ARM_LENGTH = 0.5  # meters?
+OFFSET_LIFT = 0.08
+OFFSET_RADIUS = 0.5
+
+
+def get_angles_from_quaternion(quaternion):
+    return tf.transformations.euler_from_quaternion(
+        (quaternion[0], quaternion[1], quaternion[2], quaternion[3])
+    )
 
 
 class ArucoNavigation(hm.HelloNode):
     def __init__(self):
         hm.HelloNode.__init__(self)
+        self.base_translation, self.base_orientation = None, None
+        self.tag_translation,  self.tag_orientation  = None, None
         self.main()
 
     def find_tag(self, requested_tag):
@@ -63,8 +73,8 @@ class ArucoNavigation(hm.HelloNode):
             try:
                 rospy.loginfo('LOOKING FOR THIS TAG: ')
                 rospy.loginfo(requested_tag)
-                self.tf_listener.waitForTransform(requested_tag, 'base_link', rospy.Time(), rospy.Duration(4.0))
-                self.translation, self.rotation = self.tf_listener.lookupTransform(requested_tag, 'base_link', rospy.Time(0))
+                self.tf_listener.waitForTransform('map', requested_tag, rospy.Time(), rospy.Duration(4.0))
+                self.tag_translation, self.tag_rotation = self.tf_listener.lookupTransform('map', requested_tag, rospy.Time(0))
                 rospy.loginfo("Found Requested Tag")
                 
                 found_tag = True
@@ -88,7 +98,7 @@ class ArucoNavigation(hm.HelloNode):
 
                     time.sleep(.5)
     
-            if count >= 2:
+            if count >= 4:
                 rospy.loginfo("Timed Out Looking for Tag")
                 # self.switch_to_navigation_mode()
                 return False
@@ -101,21 +111,14 @@ class ArucoNavigation(hm.HelloNode):
         print(f"Goal markers: {goal.markers}")
 
         requested_tag = "basket"
-        min_rotation = -4.05
-        max_rotation = 1.78
-        num_steps = 10
-        step = abs(min_rotation - max_rotation) / num_steps
 
         rospy.loginfo(f"LOOKING FOR THIS TAG: {requested_tag}")
-        rospy.loginfo(requested_tag)
 
         # MANIPULATION
-        self.translation, self.rotation = self.tf_listener.lookupTransform(requested_tag, 'base_link', rospy.Time(0))
-        rospy.loginfo("Found Requested Tag")
+        # self.tag_translation, self.tag_rotation = self.tf_listener.lookupTransform(requested_tag, 'base_link', rospy.Time(0))
+        # rospy.loginfo("Found Requested Tag")
         
-        found_tag = True
-
-        self.switch_to_position_mode()
+        # found_tag = True
 
         # pose = {'wrist_extension': 0.01}
         # print(f"Trying to move to pose {pose}")
@@ -129,18 +132,18 @@ class ArucoNavigation(hm.HelloNode):
         # print(f"Trying to move to pose {pose}")
         # self.move_to_pose(pose)
         
-        print(f"Trying to define target pose")
-        target_pose = PoseStamped()
-        target_pose.header.frame_id = 'basket'
+        # print(f"Trying to define target pose")
+        # target_pose = PoseStamped()
+        # target_pose.header.frame_id = 'basket'
         
-        target_pose.pose.position.x = self.translation[0]
-        target_pose.pose.position.y = self.translation[1]
-        target_pose.pose.position.z = self.translation[2]
+        # target_pose.pose.position.x = self.tag_translation[0]
+        # target_pose.pose.position.y = self.tag_translation[1]
+        # target_pose.pose.position.z = self.tag_translation[2]
 
-        target_pose.pose.orientation.x = self.rotation[0]
-        target_pose.pose.orientation.y = self.rotation[1]
-        target_pose.pose.orientation.z = self.rotation[2]
-        target_pose.pose.orientation.w = self.rotation[3]
+        # target_pose.pose.orientation.x = self.tag_rotation[0]
+        # target_pose.pose.orientation.y = self.tag_rotation[1]
+        # target_pose.pose.orientation.z = self.tag_rotation[2]
+        # target_pose.pose.orientation.w = self.tag_rotation[3]
 
 
         # NAVIGATION
@@ -150,35 +153,85 @@ class ArucoNavigation(hm.HelloNode):
         map_goal.target_pose.header.frame_id = 'map'
         map_goal.target_pose.header.stamp = rospy.Time()
         try:
-            base_translation, base_orientation = self.tf_listener.lookupTransform('base_link', 'map', rospy.Time(0))
-            self.translation, self.orientation = self.tf_listener.lookupTransform('basket', 'map', rospy.Time(0))
+            self.base_translation, self.base_orientation = self.tf_listener.lookupTransform('map', 'base_link', rospy.Time(0))
+            try:
+                translation, rotation = self.tf_listener.lookupTransform('map', 'basket', rospy.Time(0))
+            except Exception as e:
+                # Couldn't find basket in view, go with last remembered location
+                print("Error looking up map-basket transform... using historically saved coordinates")
+                
+                if self.tag_translation is not None and self.tag_orientation is not None:
+                    translation, rotation = self.tag_translation, self.tag_orientation
+                else:
+                    # Use hardcoded memory
+                    if self.find_tag("basket"):
+                        translation, rotation = self.tf_listener.lookupTransform('map', 'basket', rospy.Time(0))
+                    else: 
+                        translation, rotation = ([6.7, -3.28, 0], [0, 0, 0.96, 0.28])
+
+                
+            self.tag_translation, self.tag_orientation = translation, rotation
     
+            joint_lift = self.tag_translation[2] + OFFSET_LIFT
+            pose = {'joint_lift': joint_lift}
+            print(f"Lifting arm {pose}")
+            self.move_to_pose(pose)
+
             print(f"Trying to define map_goal")
 
-            length = math.sqrt((self.translation[0] - base_translation[0])**2 + (self.translation[1] - base_translation[1] - base_translation[1])**2)
+            rospy.loginfo(f"Base wrt Map: {self.base_translation}")
+            rospy.loginfo(f"Basket wrt Map: {self.tag_translation}")
+            rospy.loginfo(f"Base Orientation: {self.base_orientation}, as angles: {get_angles_from_quaternion(self.base_orientation)}")
+            rospy.loginfo(f"Basket Orientation: {self.tag_orientation}, as angles: {get_angles_from_quaternion(self.tag_orientation)}")
+
+            length = math.sqrt((self.tag_translation[0] - self.base_translation[0])**2 + (self.tag_translation[1] - self.base_translation[1] - self.base_translation[1])**2)
             if length > ARM_LENGTH:
                 multiplicative_ratio = (length - ARM_LENGTH) / length
-                x = base_translation[0] + multiplicative_ratio * (self.translation[0] - base_translation[0])
-                y = base_translation[1] + multiplicative_ratio * (self.translation[1] - base_translation[1])
+                x = self.base_translation[0] + multiplicative_ratio * (self.tag_translation[0] - self.base_translation[0])
+                y = self.base_translation[1] + multiplicative_ratio * (self.tag_translation[1] - self.base_translation[1])
             else: 
-                x = base_translation[0]
-                y = base_translation[1]
+                x = self.base_translation[0]
+                y = self.base_translation[1]
+            
+            eul = get_angles_from_quaternion(self.tag_orientation)
+            angle_z = eul[2]
+            quat = tf.transformations.quaternion_from_euler(0.0, 0.0, angle_z)
     
             map_goal.target_pose.pose.position.x = x
             map_goal.target_pose.pose.position.y = y
             map_goal.target_pose.pose.position.z = 0.0
-            
-            eul = tf.transformations.euler_from_quaternion((map_goal.target_pose.pose.orientation.x, map_goal.target_pose.pose.orientation.y, map_goal.target_pose.pose.orientation.z, map_goal.target_pose.pose.orientation.w))
-            quat = tf.transformations.quaternion_from_euler(0.0, 0.0, eul[2])
             map_goal.target_pose.pose.orientation.x = quat[0]
             map_goal.target_pose.pose.orientation.y = quat[1]
             map_goal.target_pose.pose.orientation.z = quat[2]
             map_goal.target_pose.pose.orientation.w = quat[3]
     
+            rospy.loginfo(f"Nav goal, approach 1: {[x, y, math.degrees(angle_z)]}")
+
+
+            angle_z_tag_normal = (angle_z + 3*pi/2 + pi) % (2*pi) - pi  # keep in the range [-pi,pi]
+            angle_z_base_direction = (angle_z + pi + pi) % (2*pi) - pi  # keep in the range [-pi,pi]
+            rospy.loginfo(f"angle_z_tag_normal: {angle_z_tag_normal} ({math.degrees(angle_z_tag_normal)})")
+            rospy.loginfo(f"angle_z_base_direction: {angle_z_base_direction} ({math.degrees(angle_z_base_direction)})")
+
+
+            quat = tf.transformations.quaternion_from_euler(0.0, 0.0, angle_z_base_direction)
+            
+            goal_x = self.tag_translation[0] + OFFSET_RADIUS * math.cos(angle_z_tag_normal)
+            goal_y = self.tag_translation[1] + OFFSET_RADIUS * math.sin(angle_z_tag_normal)
+            map_goal.target_pose.pose.position.x = goal_x
+            map_goal.target_pose.pose.position.y = goal_y
+            map_goal.target_pose.pose.position.z = 0.0
+            map_goal.target_pose.pose.orientation.x = quat[0]
+            map_goal.target_pose.pose.orientation.y = quat[1]
+            map_goal.target_pose.pose.orientation.z = quat[2]
+            map_goal.target_pose.pose.orientation.w = quat[3]
+            rospy.loginfo(f"Nav goal, approach 2: {[self.tag_translation[0] + OFFSET_RADIUS * math.cos(angle_z_tag_normal), self.tag_translation[1] + OFFSET_RADIUS * math.sin(angle_z_tag_normal), math.degrees(angle_z_base_direction)]}")
+
+    
             rospy.loginfo(f"Sending navigation map_goal {map_goal.target_pose.pose}!")
             self.client.send_goal_and_wait(map_goal)
             rospy.loginfo("DONE!")
-
+            
             time.sleep(10)
 
         except Exception as e:
